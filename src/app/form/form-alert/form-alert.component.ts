e7a808afab85d68a50a370bac4fb00bf09b93edc
weverton <wevertonprocopio690@gmail.com>
2020-08-26 08:28:33 -0300

    erro do arquivo form-alert corrigido

--- src/app/form/form-alert/form-alert.component.ts
+++ src/app/form/form-alert/form-alert.component.ts
@@ -6,13 +6,13 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./form-alert.component.scss'],
 })
 export class FormAlertComponent implements OnInit {
-  @Input invalid: boolean;
-  @Input zeroField: boolean;
-  @Input check: boolean;
+  @Input() invalid: boolean;
+  @Input() zeroField: boolean;
+  @Input() check: boolean;
 
-  @Input invalidMessage: string;
-  @Input zeroFieldMessage: string;
-  @Input checkMessage: string;
+  @Input() invalidMessage: string;
+  @Input() zeroFieldMessage: string;
+  @Input() checkMessage: string;
   constructor() {}
 
   ngOnInit(): void {}
